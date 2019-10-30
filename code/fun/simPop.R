a85bbaf39c309866a937b361dd6a45f7b12b4b98
Doug Leasure <doug.leasure@gmail.com>
2019-10-30 15:42:14 +0000

    Update simPop.R

--- code/fun/simPop.R
+++ code/fun/simPop.R
@@ -6,19 +6,26 @@ simPop <- function(n=1e3, med=100, sigma=50, beta=0.2, maxarea=1, maxpop=Inf){
   
   # covariates
   x <- rnorm(n, 0, 1)
-  area <- runif(n, 1, maxarea)
-  
+
   # model
   med <- alpha + beta * x
   D <- rlnorm_trunc(n, med, sigma, maxpop)
-  N <- D * area
+  
+   # area
+  area <- sort(runif(n, 1, maxarea))
+  orderD <- order(D)
+  A <- rep(NA,n)
+  for(i in 1:n) A[orderD[i]] <- area[i]
+  
+  # population total
+  N <- D * A
   
   # replace duplicates
   i <- which(duplicated(N) | duplicated(D))
   while(length(i) > 0){
     med <- alpha + beta * x[i]
     D[i] <- rlnorm_trunc(length(i), med, sigma, maxpop)
-    N[i] <- D[i] * area[i]
+    N[i] <- D[i] * A[i]
     
     i <- which(duplicated(N) | duplicated(D))
   }
